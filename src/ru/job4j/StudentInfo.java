9155dd2309a1f43e0d174af0f0fd6fc0d6682f5e
DS2410 <delasko316@gmail.com>
2021-02-09 19:08:08 +0400

    3.3. Разбор первой программы. [#351501]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Schelochkov Dima");
+        System.out.println("24.10.1990");
+
+    }
+}
