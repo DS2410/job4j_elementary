01baca16fb112234bd1edcedf785487a0ede120b
DS2410 <delasko316@gmail.com>
2021-02-09 19:09:46 +0400

    3.3. Разбор первой программы. [#351501]

--- src/ru/job4j/StudentInfo.java
+++ src/ru/job4j/StudentInfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println("Schelochkov Dima");
+        System.out.println("Shelochkov Dima");
         System.out.println("24.10.1990");
 
     }
