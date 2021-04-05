06dd40d807021345819255d02ead74fd1548df44
DS2410 <delasko316@gmail.com>
2021-04-05 20:09:14 +0400

    5.0.1. Сумма чисел. [#351543]

---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        Assert.assertEquals(expected, result);
+    }
+    @Test
+    public void whenSumEvenNumbersFromTwoToFourThenSix() {
+        int start = 2;
+        int finish = 4;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 6;
+        Assert.assertEquals(expected, result);
+    }
+}
