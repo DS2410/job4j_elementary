6577cc58052e44dbcab5876c6ff6e9291ab3c180
DS2410 <delasko316@gmail.com>
2021-04-07 19:24:50 +0400

    5.4. Протеиновая диета [#351538]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -7,7 +7,8 @@ public class Fitness {
             ivan *= 3;
             nik *= 2;
             month += 1;
-        } return month;
+        }
+        return month;
     }
 
     public static void main(String[] args) {
