4236eeffcf79b296b89b3caa1e4ee681f5379795
DS2410 <delasko316@gmail.com>
2021-05-20 20:14:50 +0400

    1.1. Что такое import [#504773]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -4,7 +4,7 @@ public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
         if (x1 - y1 == x2 - y2) {
-            rsl = Math.abs((x2 + y2) - (x1 - y1));
+            rsl = Math.abs(x2 - x1);
         }
         return rsl;
     }
